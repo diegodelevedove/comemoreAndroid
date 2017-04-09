134f9eea5dced6fe862c8b73ae1e7db6b54685c9
diegodelevedove <diegodelevedov@gmail.com>
2017-04-09 14:56:17 -0300

    Telas inacabadas esperando planejamento e substituindo o modelo anterior verificar a ata lá

--- comemore/app/src/main/java/comemore/sistema/comemore/Activity_pedido.java
+++ comemore/app/src/main/java/comemore/sistema/comemore/Activity_pedido.java
@@ -3,11 +3,11 @@ package comemore.sistema.comemore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Activity_pedido extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pedido);
     }
 }
